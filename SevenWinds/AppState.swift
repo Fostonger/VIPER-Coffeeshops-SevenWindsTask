5bdd75852eebcc852bbd9f1922475acd92003799
Bulat Musin <66408714+Fostonger@users.noreply.github.com>
2023-12-27 15:25:23 +0300

    App state init

--- SevenWinds/AppState.swift
+++ SevenWinds/AppState.swift
@@ -9,11 +9,26 @@ import Foundation
 
 protocol AppStateService {
     var userCredentials: Credentials? { get }
+    var token: String? { get }
     func setCredentials(_ credentials: Credentials)
+    func setToken(_ token: String, expirationDate: Int32?)
 }
 
-class UserDefaultAppState: AppStateService {
+final class UserDefaultAppState: AppStateService {
     private (set) var userCredentials: Credentials?
+    private (set) var token: String? {
+        get {
+            if expirationDate != nil && expirationDate! <= .now {
+                _token = nil
+            }
+            return _token
+        }
+        set {
+            _token = newValue
+        }
+    }
+    private var _token: String?
+    private var expirationDate: Date?
     private let credentialsStorage: UserDefaults
     
     init(with credentialsStorage: UserDefaults) {
@@ -25,8 +40,22 @@ class UserDefaultAppState: AppStateService {
         userCredentials = credentialsStorage.object(forKey: "user_credentials") as? Credentials
     }
     
+    private func fetchToken() {
+        token = credentialsStorage.object(forKey: "token") as? String
+        expirationDate = credentialsStorage.object(forKey: "token_expiration") as? Date
+    }
+    
     func setCredentials(_ credentials: Credentials) {
         credentialsStorage.setValue(credentials, forKey: "user_credentials")
         userCredentials = credentials
+    }
+    
+    func setToken(_ token: String, expirationDate: Int32? = nil) {
+        credentialsStorage.setValue(token, forKey: "token")
+        if let expirationDate = expirationDate {
+            credentialsStorage.setValue(Date.now + TimeInterval(expirationDate), forKey: "token_expiration")
+            self.expirationDate = Date.now + TimeInterval(expirationDate)
+        }
+        self.token = token
     }
 }
