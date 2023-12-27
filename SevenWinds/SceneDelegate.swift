5f1f5a2afe48f5f606351edfa2431bf57f0eab01
Bulat Musin <66408714+Fostonger@users.noreply.github.com>
2023-12-27 15:27:05 +0300

    scene delegate init first VC depending on auth state

--- SevenWinds/SceneDelegate.swift
+++ SevenWinds/SceneDelegate.swift
@@ -6,17 +6,28 @@
 //
 
 import UIKit
+import Alamofire
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let appState = UserDefaultAppState(with: UserDefaults.standard)
+        let apiClient = SevenWindsAPIClient(with: AF, token: appState.token)
+        
+        window = UIWindow(windowScene: scene)
+        window?.backgroundColor = .white
+        
+        let loginHandler: (Credentials) -> () = { _ in
+            let VC = UIViewController()
+            VC.view.backgroundColor = .red
+            self.window?.rootViewController = VC
+        }
+        
+        window?.rootViewController = LoginRouter.createModule(loginHandler: loginHandler, apiClient: apiClient, credService: appState)
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
